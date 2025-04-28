e5b087cc6d922c8a410f3477c3e9db027addd1cb
Baturalp Sönmez <baturalpsonmez2002@gmail.com>
2025-04-28 23:35:55 +0300

    Add insta_image_viewer dependency and update ProfileHeader widget
    - Added insta_image_viewer package to pubspec.yaml for image viewing functionality.
    - Refactored ProfileHeader widget to utilize InstaImageViewer for displaying user avatars, enhancing the user interface with improved image handling.

--- lib/widgets/profile_header.dart
+++ lib/widgets/profile_header.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:insta_image_viewer/insta_image_viewer.dart';
 import '../models/user_model.dart';
 
 class ProfileHeader extends StatelessWidget {
@@ -11,24 +12,30 @@ class ProfileHeader extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final avatar = CircleAvatar(
+      radius: 80,
+      backgroundColor: Theme.of(context).colorScheme.primary,
+      backgroundImage:
+          userData.photoURL != null ? NetworkImage(userData.photoURL!) : null,
+      child: userData.photoURL == null
+          ? Icon(
+              Icons.person,
+              size: 80,
+              color: Colors.white,
+            )
+          : null,
+    );
+
     return Container(
       padding: const EdgeInsets.all(16.0),
       child: Column(
         children: [
-          CircleAvatar(
-            radius: 50,
-            backgroundColor: Theme.of(context).colorScheme.primary,
-            backgroundImage: userData.photoURL != null
-                ? NetworkImage(userData.photoURL!)
-                : null,
-            child: userData.photoURL == null
-                ? const Icon(
-                    Icons.person,
-                    size: 50,
-                    color: Colors.white,
-                  )
-                : null,
-          ),
+          userData.photoURL != null
+              ? InstaImageViewer(
+                  imageUrl: userData.photoURL!,
+                  child: avatar,
+                )
+              : avatar,
           const SizedBox(height: 16),
           Text(
             userData.displayName ?? 'Anonymous',
